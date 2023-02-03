07a7d2674964ac7c0fd5d3d79c20009030c1686b
byzz <eyedroot@gmail.com>
2023-02-03 21:52:19 +0900

    update: MemberScreen

--- lib/screens/member/member_screen.dart
+++ lib/screens/member/member_screen.dart
@@ -1,12 +1,124 @@
 import 'package:flutter/material.dart';
+import 'package:mueynail/constants/color.dart';
+import 'package:mueynail/constants/style.dart';
+import 'package:mueynail/constants/value.dart';
+import 'package:mueynail/screens/home/components/home_app_bar.dart';
 
 class MemberScreen extends StatelessWidget {
   const MemberScreen({Key? key}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
-    return const Center(
-      child: Text('멤버쉽'),
+    return Scaffold(
+      appBar: const HomeAppBar(),
+      body: ListView(
+        children: [
+          userNamePart(),
+          pointPart(),
+          Padding(
+            padding: const EdgeInsets.symmetric(horizontal: 30),
+            child: menuPart(context),
+          ),
+        ],
+      ),
+    );
+  }
+
+  Widget userNamePart() {
+    return Padding(
+      padding: const EdgeInsets.symmetric(vertical: 30, horizontal: 60),
+      child: Row(
+        mainAxisAlignment: MainAxisAlignment.spaceBetween,
+        children: [
+          Row(children: [
+            Text('비즈', style: titleTextStyle.copyWith(fontSize: 24)),
+            const SizedBox(width: 5),
+            Text('고객님', style: summaryTextStyle.copyWith(fontSize: 20)),
+          ]),
+          Icon(Icons.settings, size: 24, color: Colors.black87.withOpacity(0.5)),
+        ],
+      ),
+    );
+  }
+
+  Widget pointPart() {
+    final subTitle = summaryTextStyle.copyWith(color: Colors.white.withOpacity(0.6), fontSize: 14);
+    final valueTitle = brandTextStyle.copyWith(color: Colors.white);
+
+    return Padding(
+      padding: const EdgeInsets.only(left: 30, right: 30, bottom: 30),
+      child: Container(
+        height: 100,
+        decoration: BoxDecoration(
+          color: primaryColor,
+          borderRadius: BorderRadius.circular(20),
+        ),
+        child: Padding(
+          padding: const EdgeInsets.all(10),
+          child: Row(
+            crossAxisAlignment: CrossAxisAlignment.stretch,
+            mainAxisAlignment: MainAxisAlignment.spaceAround,
+            children: [
+              Column(
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: [
+                  Text('방문', style: subTitle),
+                  Text('11', style: valueTitle),
+                ],
+              ),
+              Column(
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: [
+                  Text('멤버쉽', style: subTitle),
+                  Text('1 / 3', style: valueTitle),
+                ],
+              ),
+              Column(
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: [
+                  Text('포인트', style: subTitle),
+                  Text('4000', style: valueTitle),
+                ],
+              ),
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+
+  Widget menuPart(BuildContext context) {
+    return Column(
+      crossAxisAlignment: CrossAxisAlignment.start,
+      children: [
+        _menuPartTitle(valueBrand),
+        _menuPartButton(icon: Icons.history, label: '예약 내역'),
+        _menuPartButton(icon: Icons.article_outlined, label: '후기 작성'),
+        _menuPartButton(icon: Icons.favorite_outline, label: '관심 아트'),
+        const SizedBox(height: 15),
+        _menuPartTitle('기타'),
+        _menuPartButton(icon: Icons.person_search_outlined, label: '친구 초대', description: '(5000 포인트 지급)'),
+        _menuPartButton(icon: Icons.exit_to_app_outlined, label: '회원 탈퇴'),
+      ],
+    );
+  }
+
+  Widget _menuPartTitle(String title) {
+    return Text(title, style: normalTextStyle.copyWith(color: Colors.black54, fontSize: 14));
+  }
+
+  Widget _menuPartButton({required IconData icon, required String label, String description = ''}) {
+    return TextButton(
+      onPressed: () {},
+      child: Row(
+        children: [
+          Icon(icon),
+          const SizedBox(width: 7),
+          Text(label, style: const TextStyle(fontSize: 16, fontWeight: FontWeight.w500)),
+          const SizedBox(width: 7),
+          Text(description, style: summaryTextStyle)
+        ],
+      ),
     );
   }
 }
